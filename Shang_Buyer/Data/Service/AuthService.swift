fc5a8194a0e984360352bf4d0ba55293d0d53c82
Louis hoàng <thang.uha236@gmail.com>
2024-09-18 17:02:16 +0700

    add auth service

---
+++ Shang_Buyer/Data/Service/AuthService.swift
@@ -0,0 +1,40 @@
+//
+//  AuthService.swift
+//  Shang_Buyer
+//
+//  Created by Louis Macbook on 18/09/2024.
+//
+
+import Alamofire
+import Combine
+
+protocol AuthService {
+    func login(username: String, password: String) -> AnyPublisher<String, Error>
+}
+
+class AuthServiceImpl: AuthService {
+    func login(username: String, password: String) -> AnyPublisher<String, Error> {
+        let loginURL = "https://api.example.com/login"
+        let parameters: [String: String] = ["username": username, "password": password]
+
+        return Future { promise in
+            AF.request(loginURL, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+                .validate()
+                .responseDecodable(of: LoginResponse.self) { response in
+                    switch response.result {
+                    case .success(let loginResponse):
+                        TokenManager.shared.saveAccessToken(loginResponse.token)
+                        promise(.success(loginResponse.token))
+                    case .failure(let error):
+                        promise(.failure(error))
+                    }
+                }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+// Define your response model
+struct LoginResponse: Decodable {
+    let token: String
+}
