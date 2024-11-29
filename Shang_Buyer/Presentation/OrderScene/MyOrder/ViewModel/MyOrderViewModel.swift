6b46d47644f2ad3af874340a4aad90ec967b0ac5
ThangHT <thang.uha236@gmail.com>
2024-11-29 17:30:43 +0700

    checkpoint switch lap

---
+++ Shang_Buyer/Presentation/OrderScene/MyOrder/ViewModel/MyOrderViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MyOrderViewModel.swift
+//  Shang_Buyer
+//
+//  Created by ThangHT on 29/11/2024.
+//
+
+import Foundation
+
+
+class MyOrderViewModel: ObservableObject {
+    var orderList: [Order] = []
+    
+    
+}
